926dec25c16e2fb89de288457389c0241e33284e
Julián Palomo <julian@usemarkup.com>
2020-05-08 00:53:20 +0100

    Added file system, a couple of post and a query

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,32 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout/layout';
+import usePosts from '../hooks/use-post';
 
 // if (process.env.NODE_ENV !== 'production') {
 //   var axe = require('react-axe');
 //   axe(React, ReactDOM, 1000);
 // }
 
-const Index = () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello world</p>
-    <Link to="/about">Learn about me &rarr;</Link>
-  </Layout>
-);
+const Index = () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello world</p>
+      <Link to="/about">Learn about me &rarr;</Link>
+
+      <h2>Read my posts</h2>
+      {posts.map(post => (
+        <>
+          <pre>
+            <code>{JSON.stringify(post, null, 2)}</code>
+          </pre>
+        </>
+      ))}
+    </Layout>
+  );
+};
 
 export default Index;
